46ab6d1ce8e9510259bd49bb61d301b2e2dca2e7
Luis Lituma <luislituma22@gmail.com>
2025-06-21 20:22:10 -0500

    Mejorando y terminando el codigo del juego

--- juego.py
+++ juego.py
@@ -1,40 +1,45 @@
 """Este es el trabajo Autonomo para Logica de Programacion
 Autor: Luis Lituma
 """
-print("BIENVENIDO AL JUEGO PIEDRA PAPEL O TIJERA")
-print("Inicio del juego")
-print("Elige entre las opciones: piedra, papel o tijera")
+import random # Para usar en la eleccion aleatoria de la computadora
 
-intentos = 3  
 
-contador = 0
+while True: # Para que mientras sea verdadero el juego continua ejecutandose
+    
+    # Mensaje de Bienvenida y muesta opciones al usuario 
+    print("BIENVENIDO AL JUEGO PIEDRA, PAPEL O TIJERA")
+    print("El juego inicia...")
+    print("Elige entre estas opciones:")
+    print("1 - Piedra")
+    print("2 - Papel")
+    print("3 - Tijera")
+    print("0 - Salir")
 
-while contador < intentos:
-    jugador = input("Tu elección: ")
+    # Usamos el try para capturar entradas no válidas en teclado
+    try:
+        eleccion_Usuario = int(input("Tu eleccion: ")) # Variable que almacena la eleccion del usuario
+        if eleccion_Usuario == 0:
+            print("Saliendo del juego...")
+            break
+        if eleccion_Usuario not in[1, 2, 3]:
+            print("Opcion no válida, intenta de nuevo")
+            continue
+    except ValueError:
+        print("Entrada no válida, debes escribir solamente numeros")
+        continue
+    
+    # Eleccion random de la computadora
+    eleccion_Computadora = random.randint(1, 3) # Almacena la eleccionde la computadora. Ademas elige una opción random entre 1 y 3
 
-    # El jugador
-    print("Elegiste:", jugador)
+    opciones = {1: "Piedra", 2:"Papel", 3:"Tijera"} # Uso de diccionario para almancenar las opciones
 
-    # La computadora
-    if contador == 0:
-        computadora = "piedra"
-    elif contador == 1:
-        computadora = "papel"
+    print(f"Tu elegiste : {opciones[eleccion_Usuario]}") #Imprime la eleccion del usuario
+    print(f"La computadora eligio: {opciones[eleccion_Computadora]}") # Imprime la eleccion de la computadora
+
+    #Comparacion entre las elecciones de la computadora y el usuario. Imprime el resultado
+    if eleccion_Computadora == eleccion_Usuario:
+        print("Resultado: ¡Empate!")
+    elif (eleccion_Usuario == 1 and eleccion_Computadora ==3) or (eleccion_Usuario == 2 and eleccion_Computadora == 1) and (eleccion_Usuario == 3 or eleccion_Computadora == 1):
+        print("Resultado: ¡Ganaste!")
     else:
-        computadora = "tijera"
-
-    print("La computadora eligió:", computadora)
-
-
-    if jugador == computadora:
-        print("Empate!")
-    elif (jugador == "piedra" and computadora == "tijera") or \
-         (jugador == "papel" and computadora == "piedra") or \
-         (jugador == "tijera" and computadora == "papel"):
-        print("Ganaste!")
-    else:
-        print("Perdiste!")
-
-    contador = contador + 1
-
-print("El juego termina")
+        print("Resultado: ¡Perdiste!")
